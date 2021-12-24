90638cc5ca33b6f575364e0e6f2a1844ad41d3d2
HaneesH1994 <94160411+HaneesH1994@users.noreply.github.com>
2021-12-24 20:21:45 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,28 +1,17 @@
-pipeline {
-    agent any
-
-    tools {
-        maven "Maven"
-    }
-
-    stages {
-        stage('Build the code') {
-            steps {
-                echo "cloning the code"
-                git 'https://github.com/HaneesH1994/mvnproj.git'
-                echo "Building the code"
-                sh "mvn clean package install"
-            }
-
-        }
-        stage('deploy the code') {
-            steps {
-                sh '''
-                pwd
-                ls -l
-                sudo cp /var/lib/jenkins/.m2/repository/DEVOPS/MAVEN/2.5/MAVEN-2.5.war /var/lib/tomcat8/webapps/
-                '''
-            }
-        }
-    }
+node {
+    def mymvn
+	stage('CLoning the code') {
+	    git'https://github.com/HaneesH1994/mvnproj.git'
+		mymvn = tool 'Maven'
+	}
+	stage('building the code') {
+          withEnv(["usrmvn=$mymvn"]) {
+		  if (isUnix()) {
+		      sh '"$usrmvn/bin/mvn" clean package install'
+		 }
+		 else {
+		    bat(/"%MVN_HOME%\bin\mvn" -Dmaven.test.failure.ignore clean package/)
+		  }
+	  }
+	}
 }
